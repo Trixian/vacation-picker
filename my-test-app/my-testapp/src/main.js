5a20280965fe34680c4a884f4e574ad24585f9f7
Trixian <90383233+Trixian@users.noreply.github.com>
2022-09-27 10:02:02 +0200

    Upload data

--- my-test-app/my-testapp/src/main.js
+++ my-test-app/my-testapp/src/main.js
@@ -1,8 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-//bootstrap styling 
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 createApp(App).mount('#app')
---
+++ src/components/VacationPicker.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <h1>{{ header }}</h1>
+        <ul class="list-group">
+                 <li @click="countryAlert(this.country)" class="list-group-item" 
+                v-for="(country, index) in countryData.countries" 
+                :key="index">        
+
+                    <span :id="country.id" :title="country.details">
+                        {{ index }} - {{country.name}} 
+                    </span>
+            </li >
+        </ul>
+        <br>
+        <h3> Counter: {{ counter }}</h3>
+        <button @click="increment()" class="btn btn-success">+</button>
+        <button @click="decrement()" class="btn btn-danger">-</button>
+    </div>
+</template>
+
+<script>
+import countryData from '../data/CountryData'; 
+    export default {
+        name: 'VacationPicker',
+        data() {
+            return {
+            countryData,
+            header: 'Vue Vacation Picker',
+            counter:0,  
+            alertMessage: " You clicked on ",
+            } 
+        }, 
+        methods: {
+            countryAlert() {
+                alert(this.alertMessage + this.country)
+            },
+            increment() {
+            this.counter++;
+            },
+            decrement() {
+            this.counter--;
+            }
+        }  
+    }
+</script>
+
+<style scoped>
+
+</style>
